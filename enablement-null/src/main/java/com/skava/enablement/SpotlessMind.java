a6a8159bf0cc08220859560c2bf4949ea6cf4385
David W. Levine <david.levine@skava.com>
2019-06-17 02:20:10 -0400

    Init repos

---
+++ enablement-null/src/main/java/com/skava/enablement/SpotlessMind.java
@@ -0,0 +1,8 @@
+package com.skava.enablement;
+public class SpotlessMind {
+
+	public static void main(String[] args) {
+		System.out.println("It is a thing of joy to have a mind of spotless purity.");
+	}
+
+}
